04b20847019bd64ff81a697d0e1a212c2aa6ea9c
15night <misa000524@gmail.com>
2021-03-21 12:32:56 +0900

    [add]記事作成画面

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'App\Http\Controllers\PostController@index');
 Route::get('/posts/create', 'App\Http\Controllers\PostController@create');
 Route::get('/posts/{post}', 'App\Http\Controllers\PostController@show');
+
+Route::post('/posts', 'App\Http\Controllers\PostController@store');
